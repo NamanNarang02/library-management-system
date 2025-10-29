7510dc1f4680eac89fc6fe38f0ed7e4436e94e8e
Naman Narang <namannarang2007@gmail.com>
2025-10-29 10:37:37 +0530

    Refactor Calculate_Fine function for clarity

--- calculating_fine.sql
+++ calculating_fine.sql
@@ -1,14 +1,19 @@
-CREATE OR REPLACE FUNCTION Calculate_Fine(p_due_date IN DATE, p_return_date IN DATE) RETURN NUMBER IS
-  v_days_over NUMBER;
-  v_fine      NUMBER := 0;
-  v_rate_per_day NUMBER := 10; -- example: 10 currency units per day
+CREATE OR REPLACE FUNCTION Calculate_Fine(
+  p_due_date    IN DATE,
+  p_return_date IN DATE
+) RETURN NUMBER
+AS
+  v_fine_per_day NUMBER := 10; -- example fine per day
+  v_days_late    NUMBER;
+  v_total_fine   NUMBER;
 BEGIN
   IF p_return_date > p_due_date THEN
-    v_days_over := TRUNC(p_return_date) - TRUNC(p_due_date);
-    v_fine := v_days_over * v_rate_per_day;
+    v_days_late := p_return_date - p_due_date;
+    v_total_fine := v_days_late * v_fine_per_day;
   ELSE
-    v_fine := 0;
+    v_total_fine := 0;
   END IF;
-  RETURN v_fine;
+
+  RETURN v_total_fine;
 END;
 /
